78c3f5158636968033d9d10c328e90180ecb108b
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-24 14:50:12 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+/ Copyright (c) 2025 Adrina Peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina Peighambarzadeh
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+/**
+ * This function calculates the factorial of a number
+ */
+// eslint-disable-next-line no-unused-vars
+function calculateFactorial () {
+  // input
+  const number = parseInt(document.getElementById('input-number').value)
+  let result = 1
+  let counter = 1
+
+  // process
+  if (isNaN(number) || number < 0) {
+    document.getElementById('answer').innerHTML = 'Please enter a non-negative integer.'
+    return
+  }
+
+  while (counter <= number) {
+    result = result * counter
+    counter = counter + 1
+  }
+
+  // output
+  document.getElementById('answer').innerHTML = 'The factorial of ' + number + ' is: ' + result
+}
